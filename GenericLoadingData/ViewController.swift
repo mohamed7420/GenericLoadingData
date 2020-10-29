82fa19e0f56807cb0675777917bdeea92e1576a0
Mohamed osama <mohamed.osama9812@gmail.com>
2020-10-29 00:12:08 -0700

    Initial Commit

---
+++ GenericLoadingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericLoadingData
+//
+//  Created by Mohamed on 10/29/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
